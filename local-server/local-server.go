6b156dd6051505a5cf9d955faa069df89988f06a
Benjamin Peng <j100002ben@gmail.com>
2014-08-02 15:57:36 +0800

    Add blank server code. Add local server.

---
+++ local-server/local-server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+    "log"
+    "path"
+    "strings"
+    "net/http"
+)
+
+func main() {
+
+    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+    	upath := r.URL.Path
+		if !strings.HasPrefix(upath, "/") {
+			upath = "/" + upath
+			r.URL.Path = upath
+		}
+		upath = path.Clean(upath)
+		if _, err := os.Stat("./.tmp" + upath); err == nil {
+			http.ServeFile(w, r, "./.tmp" + upath)
+		} else if _, err := os.Stat("./app" + upath); err == nil {
+			http.ServeFile(w, r, "./app" + upath)
+		} else {
+			http.NotFound(w, r)
+		}
+    })
+    if err := http.ListenAndServe(":8010", nil); err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
